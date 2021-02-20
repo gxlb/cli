578af27c517c253bdf3ea3fe2780c753803f0524
AllyDale <vipally@gmail.com>
2021-02-20 15:43:34 +0800

    update

--- flag.go
+++ flag.go
@@ -13,3 +13,14 @@ type Flag struct {
 	names     []string
 	logicName string
 }
+
+type FlagName struct {
+	LogicName string   // logic name of the flag
+	Names     []string // name+aliases of the flag
+	Usage     string   // usage string
+	Required  bool     // if required
+	Hidden    bool     // hidden this flag
+	EnvVars   []string // environment values
+	FilePath  string   // file path
+	Value     Value    // value of this flag
+}
